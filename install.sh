c8fad1a8da0d0c724c8d8e5f8cacca72783d7782
sean <seanhc98@gmail.com>
2021-12-09 22:51:23 +0000

    Checks if file exists

--- install.sh
+++ install.sh
@@ -2,3 +2,23 @@
 
 echo "Cyborg R.A.T 3 Fix"
 echo "Created by seanb126"
+
+# checks if file exists
+#if [ -e /etc/X11/xorg.conf.d/50-vmmouse.conf ]
+
+FILENAME="test.txt"
+FIX="line"
+
+if [ -e test.txt ]
+then
+    echo "file exists"
+    if [ ! -z $(grep "$FIX" "$FILENAME") ] ; then
+        echo "line found"
+    else
+        "line not found"
+        echo "$FIX" > $FILENAME
+        cat $FILENAME
+    fi
+else
+    echo "nope"
+fi
