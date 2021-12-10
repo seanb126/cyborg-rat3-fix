2feb57079cd80b30b7bd5f3174e431646ba026bb
sean <seanhc98@gmail.com>
2021-12-10 01:04:16 +0000

    Added reinstall option

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/sudo sh
+
+# Constants
+FILENAME="/etc/X11/xorg.conf.d/910-rat-xx.conf"
+
+FIX='# RAT3 mouse
+Section "InputClass"
+ Identifier "Mouse Remap"
+ MatchProduct "Saitek Cyborg R.A.T.3 Mouse"
+ MatchDevicePath "/dev/input/event*"
+ Option "ButtonMapping" "1 2 3 4 5 0 0 8 9 0 0 0 13 14"
+EndSection'
 
 # this is more for appearances
 progress_bar()
@@ -15,44 +26,52 @@ echo -ne '##################################################   (100%)\r'
 echo -ne '\n'
 }
 
+install_fix()
+{
+touch $FILENAME
+echo "creating file..." ; progress_bar
+
+echo ""
+    
+echo "$FIX" > $FILENAME
+echo "Writing fix to file..."  ; progress_bar
+echo ""
+echo "Install Complete"
+echo ""
+echo "For effects to take place you must restart Xorg/X11 or restart your system"
+
+}
+
+reinstall_fix()
+{
+# removes file
+rm -rf $FILENAME
+echo "Removing previous install: " ; progress_bar
+
+# installs fix
+install_fix
+}
+
 execute_install()
 {
-FILENAME="/etc/X11/xorg.conf.d/910-rat-xx.conf"
-
-FIX='# RAT3 mouse
-Section "InputClass"
- Identifier "Mouse Remap"
- MatchProduct "Saitek Cyborg R.A.T.3 Mouse"
- MatchDevicePath "/dev/input/event*"
- Option "ButtonMapping" "1 2 3 4 5 0 0 8 9 0 0 0 13 14"
-EndSection'
 
 # if file exists
 if [ -e $FILENAME ]
 then
-    # checks if fix already added
-    if [ ! -z $(grep "$FIX" "$FILENAME") ] ; then
-        echo "Fix already installed on this device"
-    else
-        # appends the fix to pre-existing file
-        echo "$FIX" > $FILENAME
-        #cat $FILENAME
-    fi
+    echo ""
+    echo "The file appears to already be installed"
+    echo ""
+    read -p "do you wish to reinstall ? (y/N)" answer
+    case ${answer:0:1} in
+        y|Y|yes|Yes|YES )
+            reinstall_fix || error_msg
+        ;;
+        * )
+            exit
+        ;;
+    esac
 else
-    echo "file does not exist"
-    
-    touch $FILENAME
-    echo "creating file: " ; progress_bar
-
-    echo ""
-    
-    echo "$FIX" > $FILENAME
-    echo "Writing fix to file"  ; progress_bar
-    echo ""
-    echo "Install Complete"
-    echo ""
-    echo "For effects to take place you must restart Xorg/X11 or restart your system"
-
+    install_fix
 fi
 }
 
@@ -76,7 +95,6 @@ echo "See: " # insert license
 echo "
 "
 #echo "Fix based off https://wiki.archlinux.org/title/Mad_Catz_Mouse"
-
 
 echo "WARNING: It is advised that you create a system backup before proceeding!"
 echo ""
