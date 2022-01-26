04d707ef5675d6ccff5cb20eef39720b115391d7
sean <seanhc98@gmail.com>
2022-01-26 23:27:43 +0000

    added menu options

--- install.py
+++ install.py
@@ -1,12 +1,24 @@
 import os
 import platform
 import shutil
+from time import sleep
+from tqdm import tqdm
 
 from sqlalchemy import null
 
 class colors:
     ORIGINAL = '\033[0m'
     WARNING = '\033[93m'
+    ERROR = '\033[91m'
+
+
+FIX = """Section "InputClass"
+ Identifier "Saitek Cyborg R.A.T.3 Mouse Remap"
+ MatchProduct "Saitek Cyborg R.A.T.3 Mouse"
+ MatchDevicePath "/dev/input/event*"
+ MatchIsPointer "True"
+ Option "ButtonMapping" "1 2 3 4 5 0 0 8 9 0 0 0 13 14"
+EndSection"""
 
 def check_system(): # Checks if system is Linux
     OS = platform.system()
@@ -15,27 +27,52 @@ def check_system(): # Checks if system is Linux
         print("Operation Cancelled")
         exit
 
-def yes_no():
-    usr_input = input("Do you wish to proceed? (Y/n) ")
+# the yes/no function is used to determine if the program
+# - should continue to execute, with a no response closing the script
+def yes_no(question):
+    usr_input = input(f"{question}? (Y/n) ")
     # user responds yes
     if usr_input in ['y','Y','yes', 'Yes', 'YES']:
-        print('Answer was Yes')
+        pass
     elif usr_input == "": # if user pressed enter
-        print("Enter was pressed")
+        pass
     elif usr_input in ['n', 'N', 'no', 'No', 'NO']:
-        print("no was pressed")
         exit
     else:
         print("Input not recognised. Try again")
-        yes_no()
+        yes_no(question)
 
 def check_file_exists():
-    if not os.path.exists('test.txt'):
+    if not os.path.exists('text.txt'):
         # initiate create file process
         print("file doesnt exist")
-    elif os.path.exists('test.txt'):
-        print("File Exists")
+        create_fix()
+    elif os.path.exists('text.txt'):
+        print('It appears that the fix has previously been installed')
+        yes_no("Do you wish to reinstall")
+        print("\n")
 
+        # program will of closed if user inputted 'no'
+
+        print("Removing old installation")
+        os.remove("text.txt")
+        print(("DONE").center(cols))
+
+        create_fix()
+
+def create_fix():
+    try:
+        # raise Exception # tests try-catch
+        print("Installing fix")
+        file = open('text.txt', 'w+')
+        print("DONE")
+        file.write(FIX)
+        file.close()
+        print("Process Complete")
+    except:
+        print(f"{colors.ERROR}ERROR! There was an issue installing the fix{colors.ORIGINAL}\n")
+        print("If this was unexpected: Please report the issue to 'github.com/seanb126/cyborg-rat3-fix/issues'")
+        exit
 
 
 
@@ -56,5 +93,6 @@ if __name__ == '__main__':
     # Warning to user
     print((f"{colors.WARNING}WARNING: It is advised that you backup your system before installing!{colors.ORIGINAL}\n").center(cols))
 
-    yes_no()
+
+    yes_no("Do you wish to proceed")
     check_file_exists()
